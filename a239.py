dd87b4e808ca9ec92fae8e4ca1860f76a4c75921
Jackie Ma <jacknet.ma@gmail.com>
2020-04-27 14:13:31 +0800

    Refactor

--- a239.py
+++ a239.py
@@ -1,4 +1,6 @@
 # https://leetcode.com/problems/sliding-window-maximum/
+
+import unittest
 from typing import List
 
 
@@ -55,3 +57,17 @@ class Solution:
             if i >= k - 1:
                 res.append(nums[window[0]])
         return res
+
+
+class TestSliding(unittest.TestCase):
+
+    def get_maxs_by_sliding(self, nums, window_size):
+        return SlidingWindow(nums, window_size).get_maxs_by_sliding()
+
+    def test_sliding_window(self):
+        self.assertEqual([], self.get_maxs_by_sliding([], 3))
+        self.assertEqual([], self.get_maxs_by_sliding([1], 3))
+        self.assertEqual([], self.get_maxs_by_sliding([1, 4], 3))
+        self.assertEqual([3], self.get_maxs_by_sliding([1, 3, -1], 3))
+        self.assertEqual([3, 3, 5, 5, 6, 7], self.get_maxs_by_sliding([1, 3, -1, -3, 5, 3, 6, 7], 3))
+

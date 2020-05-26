4dea468bb3448cddff9cae6a106a1e7721811b74
Jackie Ma <jacknet.ma@gmail.com>
2020-05-26 17:37:22 +0800

    5. Longest Palindromic Substring

---
+++ src/test5_python/test_solution1.py
@@ -0,0 +1,33 @@
+import unittest
+
+
+class Solution:
+    # dp[i][j]
+    def longestPalindrome(self, s: str) -> str:
+        if not s or len(s) == 1: return s
+
+        n = len(s)
+        dp = [[False for _ in range(n)] for _ in range(n)]
+
+        # Initialize
+        for i in range(n):
+            dp[i][i] = True
+
+        res = ''
+        for i in range(n - 1, -1, -1):
+            for j in range(i, n):
+                dp[i][j] = (s[i] == s[j] and (j - i < 3 or dp[i + 1][j - 1]))
+
+                if dp[i][j] and j - i + 1 > len(res):
+                    res = s[i:j + 1]
+
+        return res
+
+
+class MyTestCase(unittest.TestCase):
+    def test_something(self):
+        self.assertEqual("aba", Solution().longestPalindrome("babad"))
+        self.assertEqual("bb", Solution().longestPalindrome("cbbd"))
+        self.assertEqual("a", Solution().longestPalindrome("a"))
+        self.assertEqual("", Solution().longestPalindrome(""))
+        self.assertEqual("aaaa", Solution().longestPalindrome("aaaa"))

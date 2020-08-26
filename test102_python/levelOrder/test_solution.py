f18da3aefa538a6fa141ba377e33a7e04a6aa066
Jackie Ma <jacknet.ma@gmail.com>
2020-08-26 10:39:59 +0800

    Refactor

--- test102_python/levelOrder/test_solution.py
+++ test102_python/levelOrder/test_solution.py
@@ -4,20 +4,22 @@ from levelOrder.solution import TreeNode, Solution1, Solution3, Solution2
 
 from converters import Converter
 
+from lib.tests.test_trees import array_to_tree
+
 
 class TestSolution1(TestCase):
     def test_levelOrder(self):
-        root: TreeNode = Converter().array_to_tree([3, 9, 20, None, None, 15, 7])
+        root: TreeNode = array_to_tree([3, 9, 20, None, None, 15, 7])
         self.assertEqual([[3], [9, 20], [15, 7]], Solution1().levelOrder(root))
 
 
 class TestSolution2(TestCase):
     def test_levelOrder(self):
-        root: TreeNode = Converter().array_to_tree([3, 9, 20, None, None, 15, 7])
+        root: TreeNode = array_to_tree([3, 9, 20, None, None, 15, 7])
         self.assertEqual([[3], [9, 20], [15, 7]], Solution2().levelOrder(root))
 
 
 class TestSolution3(TestCase):
     def test_levelOrder(self):
-        root: TreeNode = Converter().array_to_tree([3, 9, 20, None, None, 15, 7])
+        root: TreeNode = array_to_tree([3, 9, 20, None, None, 15, 7])
         self.assertEqual([[3], [9, 20], [15, 7]], Solution3().levelOrder(root))

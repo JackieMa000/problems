801122e2c7be8b6533751d98bf1bbc75e430674a
Jackie Ma <jacknet.ma@gmail.com>
2020-09-10 12:19:05 +0800

    Python tree package

--- DSA/Python/src/lib/trees/tests/test_bt.py
+++ DSA/Python/src/lib/trees/tests/test_bt.py
@@ -1,17 +1,13 @@
 from typing import List
-from unittest import skip
+from unittest.case import skip
 
 from lib.lists.arrays import Array
-from lib.trees import BinaryTree, BinarySearchTree
+from lib.trees.bt import BinaryTree
+from lib.trees.tests.test_tree import TreeTestFX
 from nodes import BinaryTreeNode
-from testing import DSATestCase
 
 
-class TreeTestCase(DSATestCase):
-    pass
-
-
-class BinaryTreeTestCase(TreeTestCase):
+class BinaryTreeTestFX(TreeTestFX):
 
     @staticmethod
     def array_to_tree(ary: List[int]) -> BinaryTreeNode:
@@ -22,12 +18,12 @@ class BinaryTreeTestCase(TreeTestCase):
         return BinaryTree(root).get_node_by_value(val)
 
 
-class DepthTestCase(BinaryTreeTestCase):
+class DepthTestCase(BinaryTreeTestFX):
     def test_case1(self):
         self.assertEqual(3, BinaryTree(Array([3, 1, 5, 0, 2]).to_binary_tree()).depth())
 
 
-class ToArrayTestCase(BinaryTreeTestCase):
+class ToArrayTestCase(BinaryTreeTestFX):
 
     def test_case1(self):
         array: List[int] = [3, 1, 5, 0, 2]
@@ -43,14 +39,14 @@ class ToArrayTestCase(BinaryTreeTestCase):
         self.assertEqual([5, 1, 4, 0, 0, 3, 6], BinaryTree(root).to_array())
 
 
-class GetArraySizeForTreeTestCase(BinaryTreeTestCase):
+class GetArraySizeForTreeTestCase(BinaryTreeTestFX):
 
     def test_case1(self):
         root: BinaryTreeNode = Array([3, 1, 5, 0, 2]).to_binary_tree()
         self.assertEqual(7, BinaryTree._get_array_size_for_tree(root))
 
 
-class GetNodeByIndexTestCase(BinaryTreeTestCase):
+class GetNodeByIndexTestCase(BinaryTreeTestFX):
 
     def test_case1(self):
         self.assertIsNone(self.get_tree_node_by_idx(BinaryTreeNode(), 2))
@@ -69,7 +65,7 @@ class GetNodeByIndexTestCase(BinaryTreeTestCase):
         return BinaryTree(root).get_node_by_index(i)
 
 
-class GetNodeByValueTestCase(BinaryTreeTestCase):
+class GetNodeByValueTestCase(BinaryTreeTestFX):
 
     def test_case1(self):
         self.assertIsNone(self.get_tree_node_by_val(BinaryTreeNode(), 2))
@@ -86,7 +82,7 @@ class GetNodeByValueTestCase(BinaryTreeTestCase):
         return BinaryTree(root).get_node_by_value(val)
 
 
-class SizeTestCase(BinaryTreeTestCase):
+class SizeTestCase(BinaryTreeTestFX):
 
     def test_case1(self):
         self.assertEqual(0, self.get_tree_size(None))
@@ -109,7 +105,7 @@ class SizeTestCase(BinaryTreeTestCase):
         return BinaryTree(root).size()
 
 
-class LowestCommonAncestorTestCase(BinaryTreeTestCase):
+class LowestCommonAncestorTestCase(BinaryTreeTestFX):
 
     def test_case1(self):
         root: BinaryTreeNode = self.array_to_tree([3, 5, 1, 6, 2, 0, 8, 0, 0, 7, 4])
@@ -125,83 +121,3 @@ class LowestCommonAncestorTestCase(BinaryTreeTestCase):
 
     def get_lowestCommonAncestor(self, root, p, q):
         return BinaryTree(root).lowestCommonAncestor(p, q)
-
-
-class BinarySearchTreeTestCase(BinaryTreeTestCase):
-    pass
-
-
-class PreorderTestCase(BinarySearchTreeTestCase):
-
-    def test_case1(self):
-        root: BinaryTreeNode = Array([2, 1]).to_binary_tree()
-        self.assertEqual([2, 1], BinarySearchTree(root).preorder())
-
-    def test_case2(self):
-        root: BinaryTreeNode = Array([2, 1, 3]).to_binary_tree()
-        self.assertEqual([2, 1, 3], BinarySearchTree(root).preorder())
-
-    def test_case3(self):
-        root: BinaryTreeNode = Array([3, 1, 5, 0, 2]).to_binary_tree()
-        self.assertEqual([3, 1, 2, 5], BinarySearchTree(root).preorder())
-
-
-class InorderTestCase(BinarySearchTreeTestCase):
-
-    def test_case1(self):
-        root: BinaryTreeNode = Array([2, 1]).to_binary_tree()
-        self.assertEqual([1, 2], BinarySearchTree(root).inorder())
-
-    def test_case2(self):
-        root: BinaryTreeNode = Array([2, 1, 3]).to_binary_tree()
-        self.assertEqual([1, 2, 3], BinarySearchTree(root).inorder())
-
-    def test_case3(self):
-        root: BinaryTreeNode = Array([3, 1, 5, 0, 2]).to_binary_tree()
-        self.assertEqual([1, 2, 3, 5], BinarySearchTree(root).inorder())
-
-
-class PostorderTestCase(BinarySearchTreeTestCase):
-
-    def test_case1(self):
-        root: BinaryTreeNode = Array([2, 1]).to_binary_tree()
-        self.assertEqual([1, 2], BinarySearchTree(root).postorder())
-
-    def test_case2(self):
-        root: BinaryTreeNode = Array([2, 1, 3]).to_binary_tree()
-        self.assertEqual([1, 3, 2], BinarySearchTree(root).postorder())
-
-    def test_case3(self):
-        root: BinaryTreeNode = Array([3, 1, 5, 0, 2]).to_binary_tree()
-        self.assertEqual([2, 1, 5, 3], BinarySearchTree(root).postorder())
-
-
-class IsValidTestCase(BinarySearchTreeTestCase):
-
-    def test_case1(self):
-        self.assertTrue(BinarySearchTree(Array([3, 1, 5, None, 2]).to_binary_tree()).is_valid())
-        self.assertTrue(BinarySearchTree(Array([5, 1, 8, 0, 2, 6, 9]).to_binary_tree()).is_valid())
-        self.assertTrue(BinarySearchTree(Array([5, 1, 6, -3, 2, None, 7]).to_binary_tree()).is_valid())
-
-    def test_case2(self):
-        self.assertFalse(BinarySearchTree(Array([5, 1, 4, 0, 6, 3, 6]).to_binary_tree()).is_valid())
-        self.assertFalse(BinarySearchTree(Array([0, None, -1]).to_binary_tree()).is_valid())
-        self.assertFalse(BinarySearchTree(Array([1, 1]).to_binary_tree()).is_valid())
-
-
-class LowestCommonAncestorBST(BinarySearchTreeTestCase):
-
-    def get_lowestCommonAncestor(self, root: BinaryTreeNode, p: BinaryTreeNode, q: BinaryTreeNode) -> BinaryTreeNode:
-        return BinarySearchTree(root).lowestCommonAncestor(p, q)
-
-    def test_case1(self):
-        root: BinaryTreeNode = self.array_to_tree([6, 2, 8, 1, 4, 7, 9, 0, 0, 3, 5])
-        p: BinaryTreeNode = self.get_tree_node_by_value(root, 2)
-        q: BinaryTreeNode = self.get_tree_node_by_value(root, 8)
-        self.assertEqual(6, self.get_lowestCommonAncestor(root, p, q).val)
-
-    def test_case2(self):
-        root: BinaryTreeNode = self.array_to_tree([6, 2, 8, 1, 4, 7, 9, 0, 0, 3, 5])
-        p: BinaryTreeNode = self.get_tree_node_by_value(root, 2)
-        q: BinaryTreeNode = self.get_tree_node_by_value(root, 4)
-        self.assertEqual(2, self.get_lowestCommonAncestor(root, p, q).val)
---
+++ DSA/Python/src/lib/trees/tests/test_tree.py
@@ -0,0 +1,5 @@
+from testing import DSATestCase
+
+
+class TreeTestFX(DSATestCase):
+    pass

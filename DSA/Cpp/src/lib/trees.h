c78357cd44ac7791e72556978e5782d018979c07
Jackie Ma <jacknet.ma@gmail.com>
2020-09-13 10:15:03 +0800

    format

--- DSA/Cpp/src/lib/trees.h
+++ DSA/Cpp/src/lib/trees.h
@@ -17,14 +17,14 @@ public:
 
 class BinaryTree : Tree
 {
-protected:
-    nodes::BinaryTreeNode* root;
-
 private:
     void
     destroy(nodes::BinaryTreeNode* root);
     static nodes::BinaryTreeNode*
     lowestCommonAncestorDfs(nodes::BinaryTreeNode* root, nodes::BinaryTreeNode* p, nodes::BinaryTreeNode* q);
+
+protected:
+    nodes::BinaryTreeNode* root;
 
 public:
     BinaryTree(nodes::BinaryTreeNode* root);

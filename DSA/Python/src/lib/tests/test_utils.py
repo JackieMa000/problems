82cd13f158d38d8df0fdbe6bcdbec11872f95886
Jackie Ma <jacknet.ma@gmail.com>
2020-09-21 18:58:45 +0800

    Python: Refactor Array Strip Trailing 0s

--- DSA/Python/src/lib/tests/test_utils.py
+++ DSA/Python/src/lib/tests/test_utils.py
@@ -1,19 +1,20 @@
-from typing import List
-from unittest import skip
-
 import lib.utils
 from testing import DSATestCase
 
 
 class ArrayRstripTestCase(DSATestCase):
-    # // FixMe: Edge cases
-    def test_case1(self):
-        array: List[int] = [3, 1, 5, 0, 2, 0, 0]
-        self.assertEqual(array, lib.utils.array_rstrip(array))
+    def test_empty_array(self):
+        self.assertEqual([], lib.utils.array_rstrip([]))
 
+    def test_length1_element0(self):
+        self.assertEqual([], lib.utils.array_rstrip([0]))
 
-class ArrayRstripDfsTestCase(DSATestCase):
-    # ToDo
-    @skip('')
-    def case1(self):
-        self.fail()
+    def test_last_element_not0(self):
+        self.assertEqual([1, 2], lib.utils.array_rstrip([1, 2]))
+
+    def test_last_element0(self):
+        actual = lib.utils.array_rstrip([1, 0])
+        self.assertEqual([1], actual)
+        self.assertEqual([1], lib.utils.array_rstrip([1, 0, 0]))
+        self.assertEqual([1, 2], lib.utils.array_rstrip([1, 2, 0, 0]))
+        self.assertEqual([3, 1, 5, 0, 2], lib.utils.array_rstrip([3, 1, 5, 0, 2, 0, 0]))
--- DSA/Python/src/lib/utils.py
+++ DSA/Python/src/lib/utils.py
@@ -2,12 +2,24 @@ from typing import List
 
 
 def array_rstrip(array: List[int]) -> List[int]:
-    # // FixMe: Edge cases
-    while not array[(-1)]:
-        array.pop(-1)
+    """
+    Remove the trailing 0s from an array
+    Rule:
+    1. Array length is 0 -> return empty array
+    2. Array length is 1, the element is 0 -> return empty array
+    3. Array last element is not 0 -> return array
+    4. Array length is bigger than 1, last element is 0 -> remove the trailing 0s by loop.
+    """
+    return _array_rstrip_it(array)
+
+
+def _array_rstrip_it(array: List[int]) -> List[int]:
+    if len(array) == 0 or (len(array) == 1 and array[0] == 0): return []
+    while not array[(-1)]: array.pop(-1)
     return array
 
 
-def array_rstrip_dfs(array: List[int]) -> List[int]:
-    # ToDo
-    pass
+def _array_rstrip_dfs(array: List[int]) -> List[int]:
+    if len(array) == 0 or (len(array) == 1 and array[0] == 0): return []
+    if array[-1] != 0: return array
+    return _array_rstrip_dfs(array[:-1])

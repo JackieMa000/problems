7de82f3216852dad89870b3e7a9f3542d0fad29a
Jackie Ma <jacknet.ma@gmail.com>
2020-09-21 18:57:03 +0800

    Refactor Utils ArrayTrailing Strip

--- DSA/Java/src/lib/Utils.java
+++ DSA/Java/src/lib/Utils.java
@@ -3,22 +3,33 @@ package lib;
 import java.util.Arrays;
 
 public class Utils {
+    /**
+     * Rule:
+     * 1. array length is 0 -> empty array
+     * 2. array length is 1 and last element is 0 -> empty array
+     * 3. array last element is not 0 -> array
+     * 4. Array length is bigger than 1, last element is 0 -> remove the trailing 0s by loop.
+     */
+    //    Remove all the trailing 0s from an array
     public static int[] arrayStripTrailing(int[] array) {
-//        FixMe: Edge case
-        int n = array.length;
-        if (array[n - 1] != 0) return array;
-        int end = n - 1;
-        for (int i = n - 1; i >=0; i--) {
-            if (array[i] == 0 && (i - 1) >= 0 && array[i - 1] != 0) {
-                end = i;
-                break;
-            }
-        }
-        return Arrays.copyOf(array, end);
+        return arrayStripTrailingIt(array);
     }
 
-    public static int[] arrayStripTrailingDfs(int[] array) {
-//        ToDo
-        return array;
+    private static int[] arrayStripTrailingIt(int[] array) {
+        int n = array.length;
+        if (n == 0 || (n == 1 && array[0] == 0)) return new int[0];
+        if (array[n - 1] != 0) return array;
+
+        int newLength = n - 1;
+        while (newLength > 0 && array[newLength - 1] == 0) newLength--;
+        return Arrays.copyOf(array, newLength);
+    }
+
+    private static int[] arrayStripTrailingDfs(int[] array) {
+        int n = array.length;
+        if (n == 0 || (n == 1 && array[0] == 0)) return new int[0];
+        if (array[n - 1] != 0) return array;
+
+        return arrayStripTrailingDfs(Arrays.copyOf(array, n - 1));
     }
 }
--- DSA/Java/tests/lib/UtilsTest.java
+++ DSA/Java/tests/lib/UtilsTest.java
@@ -1,6 +1,5 @@
 package lib;
 
-import org.junit.Ignore;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -9,34 +8,28 @@ public class UtilsTest {
 
     public static class ArrayStripTrailingTest {
 
-        @Ignore("FixMe")
         @Test
-//        FixMe
         public void case1() {
             assertArrayEquals(new int[]{}, Utils.arrayStripTrailing(new int[]{}));
         }
 
-        @Ignore("FixMe")
         @Test
-//        FixMe
         public void case2() {
-            assertArrayEquals(new int[]{}, Utils.arrayStripTrailing(new int[]{0, 0}));
+            assertArrayEquals(new int[]{}, Utils.arrayStripTrailing(new int[]{0}));
         }
 
         @Test
         public void case3() {
             assertArrayEquals(new int[]{1}, Utils.arrayStripTrailing(new int[]{1}));
+            assertArrayEquals(new int[]{1, 2}, Utils.arrayStripTrailing(new int[]{1, 2}));
+        }
+
+        @Test
+        public void case4() {
             assertArrayEquals(new int[]{1}, Utils.arrayStripTrailing(new int[]{1, 0}));
             assertArrayEquals(new int[]{1}, Utils.arrayStripTrailing(new int[]{1, 0, 0}));
             assertArrayEquals(new int[]{1, 2}, Utils.arrayStripTrailing(new int[]{1, 2, 0, 0}));
-        }
-    }
-
-    public static class ArrayStripTrailingDfsTest {
-
-        @Test
-        public void arrayStripTrailingDfs() {
-//        ToDo
+            assertArrayEquals(new int[]{1, 0, 2}, Utils.arrayStripTrailing(new int[]{1, 0, 2, 0, 0}));
         }
     }
 }

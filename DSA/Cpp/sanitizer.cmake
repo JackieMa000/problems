1bcc868be306c3848b729e699f3a1e7865cf37c0
Jackie Ma <jacknet.ma@gmail.com>
2020-10-03 19:47:08 +0800

    update

--- DSA/Cpp/sanitizer.cmake
+++ DSA/Cpp/sanitizer.cmake
@@ -7,4 +7,5 @@ set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -fsanitize=address")
 set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -fsanitize=undefined")
 set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -fsanitize=integer")
 set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -fsanitize=nullability")
+
 set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -fno-omit-frame-pointer -g -O1")

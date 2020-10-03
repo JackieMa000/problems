2278bc4bcd66cb54bac0d7e0ae35008f046b9840
Jackie Ma <jacknet.ma@gmail.com>
2020-10-03 19:31:17 +0800

    Using Sanitizer for Undefined Behaviors error catching.

--- DSA/Cpp/sanitizer.cmake
+++ DSA/Cpp/sanitizer.cmake
@@ -1,7 +1,10 @@
 # Address Sanitizers
-set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -fsanitize=address -fno-omit-frame-pointer -g -O1")
+# Overlap with Valgrind
+#set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -fsanitize=address")
 
 # UndefinedBehaviourSanitizer
+# Valgrind doesn't support it.
 set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -fsanitize=undefined")
 set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -fsanitize=integer")
 set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -fsanitize=nullability")
+set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -fno-omit-frame-pointer -g -O1")

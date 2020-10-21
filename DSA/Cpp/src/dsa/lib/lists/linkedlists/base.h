9532b7a9b12da43e29399febc0826c6985d59ff5
Jackie Ma <jacknet.ma@gmail.com>
2020-10-21 12:01:55 +0800

    Extract base LinkedList

---
+++ DSA/Cpp/src/dsa/lib/lists/linkedlists/base.h
@@ -0,0 +1,28 @@
+#ifndef DSA_SRC_DSA_LIB_LISTS_LINKEDLISTS_LS_CPP_BASE_H_
+#define DSA_SRC_DSA_LIB_LISTS_LINKEDLISTS_LS_CPP_BASE_H_
+
+#include <dsa/lib/lists/list.h>
+
+namespace dsa::lib::lists::linkedlists {
+
+class Base : public List {
+
+ public:
+    ListNode *head;
+    explicit Base(ListNode *head);
+
+    static void destroy(ListNode *head);
+//    Destroy the a cyclic LinkedList. length: the length of the LinkedList
+    static void destroyCycle(ListNode *head, length_t length);
+
+    length_t length() final;
+    [[nodiscard]] virtual arrayStruct toArray() final;
+
+    [[nodiscard]] ListNode *getNodeByIndex(int idx) const;
+    [[nodiscard]] ListNode *getNodeByValue(int val) const;
+
+};
+
+}
+
+#endif //DSA_SRC_DSA_LIB_LISTS_LINKEDLISTS_LS_CPP_BASE_H_

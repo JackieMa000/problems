9d61b3b2c60b46b3a32aee893814bbeced5f78d0
Jackie Ma <jacknet.ma@gmail.com>
2020-12-09 11:57:44 +0800

    solve name conflicts with built-in names

--- DSA/Python/src/dsa/lib/math/nums/three_sum.py
+++ DSA/Python/src/dsa/lib/math/nums/three_sum.py
@@ -28,14 +28,14 @@ class ThreeSum:
             if i >= 1 and nums[i] == nums[i - 1]: continue
             left, right = i + 1, n - 1
             while left < right:
-                sum = nums[i] + nums[left] + nums[right]
-                if sum == 0:
+                sum_ = nums[i] + nums[left] + nums[right]
+                if sum_ == 0:
                     while left < right and nums[left] == nums[left + 1]: left += 1
                     while left < right and nums[right] == nums[right - 1]: right -= 1
                     result.append([nums[i], nums[left], nums[right]])
                     left += 1
                     right -= 1
-                elif sum < 0:
+                elif sum_ < 0:
                     left += 1
                 else:
                     right -= 1

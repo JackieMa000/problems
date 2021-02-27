f99718487c6dd04d5606b3d5fd86a38874689aa0
Jackie Ma <jacknet.ma@gmail.com>
2021-02-28 02:15:29 +0800

    clean

--- DSA/Cpp/cTest.sh
+++ DSA/Cpp/cTest.sh
@@ -1,4 +1,4 @@
-#cd /Users/Jackie/Dev/DSA/problems/DSA/Cpp/cmake-build-debug
+cd /Users/Jackie/Dev/DSA/problems/DSA/Cpp/cmake-build-debug
 
 #### Build All Targets ###
 /Applications/CLion.app/Contents/bin/cmake/mac/bin/cmake --build /Users/jackie/Dev/DSA/problems/DSA/Cpp/cmake-build-debug --target all -- -j 6

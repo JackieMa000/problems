836bcf3de10eeceb819008c0aa8d43462617491f
Jackie Ma <jacknet.ma@gmail.com>
2025-04-02 19:14:52 +0800

    enable C test

--- DSA/C/cmake/test.cmake
+++ DSA/C/cmake/test.cmake
@@ -2,9 +2,10 @@
 include(CTest)
 
 # Install libcheck
-# include(${EXTERNAL_DIR}/libcheck.cmake)
-# For the convenience of checking libcheck source code
-add_subdirectory(${EXTERNAL_DIR}/libcheck-src)
+include(${EXTERNAL_DIR}/libcheck.cmake)
+
+# Disable it by default, but enable it if you want to check libcheck source code
+#add_subdirectory(${EXTERNAL_DIR}/libcheck-src)
 
 include_directories(${CMAKE_SOURCE_DIR}/test)
 add_subdirectory(${CMAKE_SOURCE_DIR}/test)
